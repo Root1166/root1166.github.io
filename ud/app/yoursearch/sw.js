6e227e87670d444adead57769b8073de08961378
congthanh <congthanh5867@gmail.com>
2020-04-21 07:45:16 +0700

    update

--- ud/app/yoursearch/sw.js
+++ ud/app/yoursearch/sw.js
@@ -1,25 +1,30 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js',
+    '/js/bootstrap.min.js',
+    '/js/jquery.fancybox.min.js',
+    '/js/jquery.min.js',
+    '/js/scriptSearch.js'
+
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
 });
