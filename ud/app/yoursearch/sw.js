e50893e8f45d52fb2b0771d86828f69a9fe07733
congthanh <congthanh5867@gmail.com>
2020-04-21 07:36:26 +0700

    update

--- ud/app/yoursearch/sw.js
+++ ud/app/yoursearch/sw.js
@@ -1,32 +1,25 @@
-var cacheName = 'Search myApp';
+var cacheName = 'hello-pwa';
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '../../css/styleSearch.css',
-    ' ../../css/jquery.fancybox.min.css',
-    '/main.js',
-    '../../js/scriptSearch.js',
-    '../../js/scriptSearch.js',
-    '../../js/jquery.fancybox.min.js',
-    '../../js/jquery.min.js',
-    '../../img/icon-search.png'
-
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
 ];
 
+/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-
+/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
