d8e131b5714f6d77987c199d97847a5a3ef37f98
congthanh5867 <59190446+congthanh5867@users.noreply.github.com>
2020-05-18 10:50:46 +0700

    update

--- ud/app/yoursearch/js/scriptSearch.js
+++ ud/app/yoursearch/js/scriptSearch.js
@@ -47,7 +47,7 @@ function search() {
             q: q,
             type: 'video',
             maxResults: 6,
-            key: 'AIzaSyAe8ONxWN0rZRfgBvQhfNFijSPYzDCld28'
+            key: 'AIzaSyCXHkSMFLcwKOI2n7_V-9KWeGt_ZpUmvaE'
         },
         function(data) {
             var nextPageToken = data.nextPageToken;
@@ -86,7 +86,7 @@ function nextPage() {
             pageToken: token,
             type: 'video',
             maxResults: 6,
-            key: 'AIzaSyAe8ONxWN0rZRfgBvQhfNFijSPYzDCld28'
+            key: 'AIzaSyCXHkSMFLcwKOI2n7_V-9KWeGt_ZpUmvaE'
         },
         function(data) {
             var nextPageToken = data.nextPageToken;
@@ -127,7 +127,7 @@ function prevPage() {
             pageToken: token,
             type: 'video',
             maxResults: 6,
-            key: 'AIzaSyAe8ONxWN0rZRfgBvQhfNFijSPYzDCld28'
+            key: 'AIzaSyCXHkSMFLcwKOI2n7_V-9KWeGt_ZpUmvaE'
         },
         function(data) {
             var nextPageToken = data.nextPageToken;
